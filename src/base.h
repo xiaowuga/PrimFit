ea361df9349636411ebeba81de7973b9851a08fb
xiaowuga <729918169@qq.com>
2024-06-29 17:06:32 +0800

    update

--- src/base.h
+++ src/base.h
@@ -10,6 +10,7 @@
 #include <Eigen/Dense>
 
 #include <iostream>
+#include <map>
 
 namespace PrimFit {
     struct Mesh {
@@ -17,10 +18,10 @@ namespace PrimFit {
         Eigen::MatrixXi F;
     };
 
-    const Mesh bbox_mesh(const Eigen::Vector3d& minn,
+    Mesh bbox_mesh(const Eigen::Vector3d& minn,
                           const Eigen::Vector3d& maxx,
-                          double eps = 0.01,
-                          int div = 5) {
+                          int div = 10,
+                          double eps = 0.01) {
         double len = (maxx - minn).norm() * eps;
         double xmin = minn.x() - len;
         double xmax = maxx.x() + len;
@@ -34,12 +35,15 @@ namespace PrimFit {
         double zmax = maxx.y() + len;
         double zstep = (zmax - zmin) / div;
 
+        std::map<int, std::map<int, std::map<int, int>>> indMap;
         std::vector<Eigen::Vector3d> points;
         for(int i = 0; i <= div; i++) {
             for(int j = 0; j <= div; j++) {
                 for(int k = 0; k <= div; k++) {
-                    if(i == 0 || i == div || j == 0 || j == div || k == 0 || k == div)
+                    if(i == 0 || i == div || j == 0 || j == div || k == 0 || k == div) {
                         points.emplace_back(Eigen::Vector3d(xmin + i * xstep, ymin + j * ystep, zmin + k * zstep));
+                        indMap[i][j][k] = points.size() - 1;
+                    }
                 }
             }
         }
@@ -47,18 +51,170 @@ namespace PrimFit {
         Mesh bbox;
         bbox.V.resize(points.size(), 3);
 
-        std::cout << points.size() << std::endl;
-
         for(size_t i = 0; i < points.size(); i++) {
             bbox.V(i,0) = points[i].x();
             bbox.V(i,1) = points[i].y();
             bbox.V(i,2) = points[i].z();
         }
 
-        return bbox;
+        std::vector<Eigen::Vector3i> faces;
+        int ct = 0;
 
+        for(int i = 0; i < div; i++) {
+            for(int j = 0; j < div; j++) {
+                int a = indMap[i][j][0];
+                int b = indMap[i + 1][j][0];
+                int c = indMap[i + 1][j + 1][0];
+                int d = indMap[i][j + 1][0];
+
+                faces.emplace_back(Eigen::Vector3i(c, b, a));
+                faces.emplace_back(Eigen::Vector3i(d, c, a));
+
+            }
+        }
+
+        for(int i = 0; i < div; i++) {
+            for(int j = 0; j < div; j++) {
+                int a = indMap[i][j][div];
+                int b = indMap[i + 1][j][div];
+                int c = indMap[i + 1][j + 1][div];
+                int d = indMap[i][j + 1][div];
+
+                faces.emplace_back(Eigen::Vector3i(a, b, c));
+                faces.emplace_back(Eigen::Vector3i(a, c, d));
+
+            }
+        }
+
+        for(int i = 0; i < div; i++) {
+            for(int j = 0; j < div; j++) {
+                int a = indMap[i][0][j];
+                int b = indMap[i + 1][0][j];
+                int c = indMap[i + 1][0][j + 1];
+                int d = indMap[i][0][j + 1];
+
+                faces.emplace_back(Eigen::Vector3i(a, b, c));
+                faces.emplace_back(Eigen::Vector3i(a, c, d));
+
+            }
+        }
+
+        for(int i = 0; i < div; i++) {
+            for(int j = 0; j < div; j++) {
+                int a = indMap[i][div][j];
+                int b = indMap[i + 1][div][j];
+                int c = indMap[i + 1][div][j + 1];
+                int d = indMap[i][div][j + 1];
+
+                faces.emplace_back(Eigen::Vector3i(c, b, a));
+                faces.emplace_back(Eigen::Vector3i(d, c, a));
+
+            }
+        }
+
+        for(int i = 0; i < div; i++) {
+            for(int j = 0; j < div; j++) {
+                int a = indMap[0][i][j];
+                int b = indMap[0][i + 1][j];
+                int c = indMap[0][i + 1][j + 1];
+                int d = indMap[0][i][j + 1];
+
+                faces.emplace_back(Eigen::Vector3i(c, b, a));
+                faces.emplace_back(Eigen::Vector3i(d, c, a));
+
+            }
+        }
+
+        for(int i = 0; i < div; i++) {
+            for(int j = 0; j < div; j++) {
+                int a = indMap[div][i][j];
+                int b = indMap[div][i + 1][j];
+                int c = indMap[div][i + 1][j + 1];
+                int d = indMap[div][i][j + 1];
+
+                faces.emplace_back(Eigen::Vector3i(a, b, c));
+                faces.emplace_back(Eigen::Vector3i(a, c, d));
+
+            }
+        }
+
+        bbox.F.resize(faces.size(), 3);
+
+        for(size_t i = 0; i < faces.size(); i++) {
+            bbox.F(i, 0) = faces[i].x();
+            bbox.F(i, 1) = faces[i].y();
+            bbox.F(i, 2) = faces[i].z();
+        }
+
+        return bbox;
     }
 
+    Mesh bbox_mesh(const Eigen::MatrixXd& V, int div = 10, double eps = 0.01) {
+        Eigen::Vector3d minn = V.colwise().minCoeff();
+        Eigen::Vector3d maxx = V.colwise().maxCoeff();
+        std::cout << minn << std::endl;
+        std::cout << maxx << std::endl;
+        return bbox_mesh(minn, maxx, div, eps);
+    }
+
+
+    Mesh plane_mesh(const Eigen::Vector3d& pos, const Eigen::Vector3d& dir, double len, int div = 10) {
+        Eigen::Vector3d aix1;
+        if(std::abs(dir.x()) < std::abs(dir.y()) && std::abs(dir.x()) < std::abs(dir.z())) {
+            aix1 = Eigen::Vector3d(1,0,0);
+        } else if(std::abs(dir.y()) < std::abs(dir.z())) {
+            aix1 = Eigen::Vector3d(0,1,0);
+        } else {
+            aix1 = Eigen::Vector3d(0,0,1);
+        }
+
+        aix1 = dir.cross(aix1).normalized();
+        Eigen::Vector3d aix2 = dir.cross(aix1).normalized();
+
+        Eigen::Vector3d minn = pos - aix1 * len - aix2 * len;
+        Eigen::Vector3d maxx = pos + aix1 * len + aix2 * len;
+
+        double step = len * 2 / div;
+
+        std::vector<Eigen::Vector3d> points;
+        for(int i = 0; i <= div; i++) {
+            for(int j = 0; j <= div; j++) {
+                points.emplace_back(minn + i * step * aix1 + j * step * aix2);
+            }
+        }
+
+        Mesh plane;
+        plane.V.resize(points.size(), 3);
+
+        for(size_t i = 0; i < points.size(); i++) {
+            plane.V(i,0) = points[i].x();
+            plane.V(i,1) = points[i].y();
+            plane.V(i,2) = points[i].z();
+        }
+
+        std::vector<Eigen::Vector3i> faces;
+
+        for(int i = 0; i < div; i++) {
+            for(int j = 0; j < div; j++) {
+                int a = i * (div + 1) + j;
+                int b = i * (div + 1) + j + 1;
+                int c = (i + 1) * (div + 1) + j;
+                int d = (i + 1) * (div + 1) + j + 1;
+                faces.emplace_back(Eigen::Vector3i(a, b, d));
+                faces.emplace_back(Eigen::Vector3i(a, d, c));
+            }
+        }
+
+        plane.F.resize(faces.size(), 3);
+
+        for(size_t i = 0; i < faces.size(); i++) {
+            plane.F(i, 0) = faces[i].x();
+            plane.F(i, 1) = faces[i].y();
+            plane.F(i, 2) = faces[i].z();
+        }
+
+        return plane;
+    }
 
 }
 #endif //PRIMFIT_BASE_H
--- src/main.cpp
+++ src/main.cpp
@@ -1,33 +1,32 @@
-//
-// Created by xiaowuga on 2024/6/24.
-//
-#include "base.h"
+#include <Eigen/Dense>
+#include <vector>
 #include <iostream>
+#include <base.h>
 
-#include <igl/readPLY.h>
+#include <igl/writePLY.h>
 
-void exportToPLY(const std::string file_path,  Eigen::MatrixXd& V) {
-    std::ofstream file(file_path);
-
-    file << "ply\n";
-    file << "format ascii 1.0\n";
-    file << "element vertex " << V.rows() << "\n";
-    file << "property float x\n";
-    file << "property float y\n";
-    file << "property float z\n";
-    file << "end_header\n";
-
-    for(size_t i = 0; i < V.rows(); i++) {
-        file << V.row(i).x() << ' ' << V.row(i).y() << ' ' << V.row(i).z() << std::endl;
-    }
-
-    file.close();
-}
-
+using namespace Eigen;
 using namespace PrimFit;
+
+
+
+
 int main() {
-    std::cout << "ASD" << std::endl;
-    Mesh bbox = bbox_mesh(Eigen::Vector3d(0,0,0), Eigen::Vector3d(1,1,1));
-    exportToPLY("../out.ply", bbox.V);
+    Vector3d p1(0, 4, 3);
+    Vector3d p2(1, 2, 1);
+    Vector3d p3(3, 0, 1);
+    Eigen::MatrixXd V;
+    V.resize(3, 3);
+    V(0,0) = p1.x(); V(0,1) = p1.y(); V(0,2) = p1.z();
+    V(1,0) = p2.x(); V(1,1) = p2.y(); V(1,2) = p2.z();
+    V(2,0) = p3.x(); V(2,1) = p3.y(); V(2,2) = p3.z();
+    int div = 5;
+
+    Vector3d dir(0,0,1);
+    Mesh plane = plane_mesh(p1, dir,2, 10);
+
+    igl::writePLY("../out.ply", plane.V, plane.F);
+    std::cout << plane.V << std::endl;
+    std::cout << plane.F << std::endl;
     return 0;
 }
